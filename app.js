494d3062e277e510f3d5f5bd72b7ad2f708a53d4
luigi-giordano <luigi.giordano1992@gmail.com>
2025-01-09 12:38:52 +0100

    completed exercise

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
 const postsRouter = require('./routers/posts.js');
+const checkTime = require('./middleware/checkTime.js');
+const errorsHandler = require('./middleware/errorsHandler.js');
+const notFound = require('./middleware/notFound.js');
 
 const port = 3001;
 
 app.use(express.json());
 
-
+// app.get('/', checkTime, (req, res) => {
+//   res.send('Server dei post')
+// })
+app.use(checkTime);
 
 app.use('/posts', postsRouter);
 
+app.use(errorsHandler);
+
+app.use(notFound);
 
 app.listen(port, () => {
   console.log(`Server in ascolto su http://localhost:${port}`);
--- middleware/checkTime.js
+++ middleware/checkTime.js
@@ -2,7 +2,7 @@ function checkTime(req, res, next) {
   const date = new Date();
   const hour = date.getHours();
 
-  if(hour > 23 || hour < 18 ){
+  if(hour > 23 || hour < 12 ){
     return res.send('Server chiuso');
   }
   next();
